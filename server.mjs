aa3d6a67b196b3fa13bab027f0608fd15bd439cc
Pedro Diter <114523969+pedroditersimon@users.noreply.github.com>
2024-07-09 00:56:54 -0300

    added Origin to cors allowedHeaders

--- server.mjs
+++ server.mjs
@@ -11,7 +11,7 @@ import cors from "cors";
 app.use(cors({
   origin: ['http://localhost:3000', process.env.CORS_ORIGIN], // Replace with your frontend domain
   credentials: true, // Enable cookies and HTTP authentication
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin']
 }));
 
 app.use(morgan("dev"));
